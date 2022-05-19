48d070f2ac2abee23f19356fe53604a5b640d7ed
franTarkenton <kevin.netherton@gov.bc.ca>
2022-05-19 12:49:58 -0700

    fixed the service
    * service now delivers a stream of user pages

--- frontend/src/app/services/user-service.service.ts
+++ frontend/src/app/services/user-service.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams, JsonpClientBackend  } from '@angular/common/http';
-import { OidcClientNotification, OidcSecurityService, OpenIdConfiguration, UserDataResult } from 'angular-auth-oidc-client';
-import { Observable, of, mergeMap, switchMap, firstValueFrom, from, tap, Subscriber, take, publish  } from 'rxjs';
-import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { mergeMap, switchMap } from 'rxjs';
+//import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
 import jwtDecode, { JwtPayload } from "jwt-decode";
-import { UrlSerializer,  } from '@angular/router';
+//import { UrlSerializer,  } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -14,35 +14,33 @@ export class UserServiceService {
   httpHeader: HttpHeaders = new HttpHeaders();
   oidcURI: string = '';
   oidcAuthURI: string = '';
-  first :number = 0;
-  max : number= 100;
-
+  first: number = 0;
+  pagesize: number = 50;
 
   constructor(
     private http: HttpClient,
     public oidcSecurityService: OidcSecurityService) {
-          this.oidcSecurityService.getAccessToken().subscribe((token) => {
-              this.oidcAccessToken = token;
-              var decodedToken = jwtDecode<JwtPayload>(this.oidcAccessToken);
-              console.log("decodedToken " + this.oidcURI );
+    this.oidcSecurityService.getAccessToken().subscribe((token) => {
+      this.oidcAccessToken = token;
+      var decodedToken = jwtDecode<JwtPayload>(this.oidcAccessToken);
+      console.log("decodedToken " + this.oidcURI);
 
-              // need to tweak the url by from
-              // <KC_HOST>/auth/realms/<KC_REALM>"
-              // to
-              //<KC_HOST>/auth/admin/realms/<KC_REALM>"
-              // || in case property is not populated
-              this.oidcURI = decodedToken.iss || '';
-              this.oidcAuthURI = this.oidcURI.replace('/auth/', '/auth/admin/');
-            }
-          );
+      // need to tweak the url by from
+      // <KC_HOST>/auth/realms/<KC_REALM>"
+      // to
+      //<KC_HOST>/auth/admin/realms/<KC_REALM>"
+      // || in case property is not populated
+      this.oidcURI = decodedToken.iss || '';
+      this.oidcAuthURI = this.oidcURI.replace('/auth/', '/auth/admin/');
+    }
+    );
   }
 
-  // create an observable that sends 'first' and 'max' parameters for an api call, keep on iterating
-  // when api call returns 0 records should trigger observable complete.
-
-
-
   getHeader() {
+    /**
+     * creates a header and attached the bearer token to allow for communication
+     * with auth provider (keycloak)
+     */
     const headerDict = {
       'Content-type': 'application/json',
       'Accept': 'application/json',
@@ -52,93 +50,68 @@ export class UserServiceService {
     return httpHeads;
   };
 
-  getUserPageParams(numUsers: string) {
-    let userPageParams = [];
-    let dataNumber: number = parseInt(numUsers);
-    for (let x = 0; x < dataNumber; x+=50){
-      userPageParams.push({"first": x.toString(), "max": "100"});
-      console.log("page params: " + x)
-    }
-    //return userPageParams;
-    console.log("userPageParams : " + JSON.stringify(userPageParams));
-    return userPageParams;
-  }
-
   getTotalUsers() {
+    /**
+     * call the keycloak requesting the total number of users in the
+     * auth system
+     */
     const httpHeads: HttpHeaders = this.getHeader();
     return this.http.get(this.oidcAuthURI + '/users/count',
-        { "headers": httpHeads });
+      { "headers": httpHeads });
   }
 
   getPageParams() {
-  //async getPageParams() {
-    // let myvar = await firstValueFrom(this.getTotalUsers());
+    /**
+     * consumes the total users observable, and converts to an array
+     * containing parameters that need to be sent to future requests
+     */
     let totalUsers: any;
     //let myvar = firstValueFrom(this.getTotalUsers());
-    let myvar = this.getTotalUsers().pipe(take(1)).subscribe( param => {
-      totalUsers = param;
-    });
-    console.log("page totalUsers: " + totalUsers);
-
-    console.log("page total users: " + JSON.stringify(totalUsers));
-    let pageParms: pageParamModel[] = this.getUserPageParams(totalUsers.toString());
-    //return Observable.fromPromise(pageParms);
-    console.log("getPageParams: " + JSON.stringify(pageParms));
-    return pageParms;
+    let pageParams = this.getTotalUsers().pipe(
+      switchMap((param) => {
+        console.log("total users in keycloak: " + param)
+        let userPageParams: pageParamModel[] = [];
+        for (let x = 0; x <= param; x += this.pagesize) {
+          userPageParams.push({ "first": x.toString(), "max": this.pagesize.toString() });
+        }
+        return userPageParams;
+      })
+    );
+    return pageParams;
   }
 
   getUsersPage(first: string, max: string) {
+    /**
+     * makes individual requests using first / max as parameters that are
+     * sent to the request
+     */
     const httpHeads: HttpHeaders = this.getHeader();
     const params = new HttpParams()
-    .set("first", first )
-    .set("max", max );
+      .set("first", first)
+      .set("max", max);
     console.log('get user page params: ' + JSON.stringify(params) + ' ' + first)
-    return this.http.get(this.oidcAuthURI + '/users', { "headers": httpHeads,
-        "params": params });
+    return this.http.get(this.oidcAuthURI + '/users', {
+      "headers": httpHeads,
+      "params": params
+    });
   }
 
-// Type 'Promise<[]>' must have a '[Symbol.iterator]()' method that returns an iterator.
-// Type 'Promise<[]>' is missing the following properties from type '[]': length, pop, push, concat, and 28 more.
- getUsers() {
-    let inputParams = this.getPageParams();
-
-  //   let pageParms = new Observable<pageParamModel[]>( subscriber => {
-  //     //for (let x=0; x<inputParmas.length; x++) {}
-  //     for (const param of inputParams){
-  //       console.log("(emit new param page)param is: " + JSON.stringify(param));
-  //       subscriber.next(param);
-  //     }
-  //     subscriber.complete();
-  //  });
-
-    let pageParms: Observable<any> = from(inputParams);
-
-    //console.log("page params om getUsers: "+ JSON.stringify(inputParams) + (typeof inputParams) + ' ' + inputParams);
-    let pageParamsObservable = from(pageParms);
-    // need to combine pageParams with the http requests
+  getUsers() {
+    /**
+     * returns an observable stream that returns pages of data from the
+     * keycloak api containing individual users.
+     */
+    let pageParamsObservable = this.getPageParams();
     return pageParamsObservable.pipe(
-      tap(
-        pageParam => { console.log('page 2' + JSON.stringify(pageParam));
-                        return this.getUsersPage(pageParam['first'], pageParam.max) }
+      mergeMap(
+        pageParam => {
+          console.log('page 2' + JSON.stringify(pageParam));
+          return this.getUsersPage(pageParam['first'], pageParam.max)
+        }
       )
     );
   }
 }
-
-/*
-a) get the total number of users
-b) use to assemble an objservable
-public sample(): Observable<any[]>
-{
-  let call1=this.http.get(this._url+'/v1/sample1')
-  let call2=this.http.get(this._url+'/v1/sample2')
-  let call3=this.http.get(this._url+'/v1/sample3')
-  return forkJoin([call1, call2,call3]);
-}
-
-return that.
-
-*/
 
 interface pageParamModel {
   first: string,
